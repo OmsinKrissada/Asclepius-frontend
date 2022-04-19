9490083806a2757212f126cac2c758c0c6ff7e01
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2022-04-19 12:14:58 +0700

    Updated component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -10,9 +10,9 @@
 		<div class="flex items-center">
 			<!-- <button class="m-2 px-3 py-1 font-krub">English</button> -->
 
-			<nuxt-link to="/how_it_works">
+			<!-- <nuxt-link to="/how_it_works">
 				<button class="m-2 px-3 py-1 mdi mdi-help-circle-outline text-3xl" />
-			</nuxt-link>
+			</nuxt-link> -->
 			<!-- <button class="m-2 px-3 py-1 mdi mdi-menu mdi-close text-3xl" /> -->
 		</div>
 	</div>
