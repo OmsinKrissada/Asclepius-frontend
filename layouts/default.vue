05fd7c4ac76fa32a520166d0ef29163d3d63fff2
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2022-11-15 16:20:40 +0700

    vercel analytics attempt

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import { inject } from '@vercel/analytics';
+
+// Make sure to call this only once in your app
+inject();
+</script>
 <template>
   <div class="flex flex-col min-h-screen bg-slate-50 ">
     <Navbar />
